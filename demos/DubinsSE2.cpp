22f165108935dc5b3f87f3f92667f29fb9bff731
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2024-09-08 11:29:28 -0600

    Add dubins SE2 demo
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

---
+++ demos/DubinsSE2.cpp
@@ -0,0 +1,15 @@
+// This utility finds the dubins path between N>=2 points.
+// Optionally:
+// * Graph the results
+// * Show all 6 classifications
+// * Show the optimal
+// * Show the distances
+// * Fun extension traveling saleseman dubins
+
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+
+int main() {
+    return 0;
+}
